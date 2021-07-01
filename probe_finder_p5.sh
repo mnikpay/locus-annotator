29ec4ba41a300d2eb0bb71ef5489ba2c68f9af59
mnikpay <60237353+mnikpay@users.noreply.github.com>
2021-07-01 02:53:17 -0400

    Update probe_finder_p5.sh

--- probe_finder_p5.sh
+++ probe_finder_p5.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-#SBATCH --account=def-mcpherso
-#SBATCH -c 1                      # Number of CPUS requested. If omitted, the default is 1 CPU.
-#SBATCH --mem=10000M                  # Memory requested in megabytes. If omitted, the default is 1024 MB.
-#SBATCH --time=2:59:00 # How long will your job run for? If omitted, the default is 3 hours.
-#SBATCH -o /home/nikpay/projects/def-mcpherso/nikpay/mybulk/logs/aalog_%x-%j.out
 
 c=$1
 
