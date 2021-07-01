c5808880f2b781a9a784b3f362c0dc691e456b6e
mnikpay <60237353+mnikpay@users.noreply.github.com>
2021-07-01 17:17:03 -0400

    Update manager_p1.sh

--- manager_p1.sh
+++ manager_p1.sh
@@ -5,7 +5,7 @@
 
 #This analysis pipeline assumes trait files are in ./inputs directory and besd files are in ./besds/ directory
 
-#Example1 with genomic position: bash manager_m1.sh chr5:95665720 BMI_PMID30239722.gz
+#Example1 with genomic position: bash manager_m1.sh chr5:95665720 BMI_PMID_30239722.gz
 
 #Example2 with genomic range: bash manager_m1.sh chr15:48888460-48905309 Standing_height_UKBB.gz
 
@@ -26,7 +26,7 @@ while read line; do echo $line;bash haplotype_finder_p3.sh $line; done < $a
 
 #New besd files can be added to the end of besd_files.list
 
-while read line; do echo $line;bash probe_finder_p5.sh $line $a; done < besd_files.list
+while read line; do echo $line;bash probe_finder_p5.sh $line $a; done < ./inputs/besd.list
 
 
 while read line; do echo $line;bash MR_test_p6.sh $line $trait $a; done < $a.besd.probes
