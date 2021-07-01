5a18eaa993c45afdf106b9ef9db54c0e70db3716
mnikpay <60237353+mnikpay@users.noreply.github.com>
2021-07-01 02:52:14 -0400

    Update haplotype_finder_p3.sh

--- haplotype_finder_p3.sh
+++ haplotype_finder_p3.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
-#SBATCH --account=def-wells
-#SBATCH -c 1                      # Number of CPUS requested. If omitted, the default is 1 CPU.
-#SBATCH --mem=20000M                  # Memory requested in megabytes. If omitted, the default is 1024 MB.
-#SBATCH --time=2:59:00 # How long will your job run for? If omitted, the default is 3 hours.
-#SBATCH -o /home/nikpay/scratch/logs/aalog_%x-%j.out
-
-
-
 
 chromosome=$1
 boundary1=$2
